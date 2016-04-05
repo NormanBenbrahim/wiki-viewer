9485d53efcb05c255499dbf5fa3d2ca55f58b6e1
NormanBenbrahim <norman.benbrahim@gmail.com>
2016-04-04 22:02:15 -0400

    added hover for button and link, as well as tested selectors for js interaction

--- interact.js
+++ interact.js
@@ -1,3 +1,30 @@
-// attempting APIs without the use of jquery
-var xhr = new XMLHttpRequest(); //initialize
+// ------- attempting APIs without the use of jquery -----------
 
+
+
+// endpoint for api
+var api_endpoint = "https://en.wikipedia.org/w/api.php" 
+
+// either the user clicks on the button after typing, or types enter
+
+// the user pushes enter
+function searchWikiEnter(e) {
+	if (e.keyCode===13 && document.getElementById('searchBox').value!=="") {
+		// store in variable
+		var search_words = document.getElementById('searchBox').value;
+		console.log('The search word(s): ' + search_words); // for debugging
+
+		// initialize http request
+		var xhr = new XMLHttpRequest(); 
+		var api_endpoint = "https://en.wikipedia.org/w/api.php"; 
+		
+	}
+}
+
+// the user clicks the button
+function searchWikiClick(){
+	if (document.getElementById('searchBox').value!=="") {
+		var search_words = document.getElementById('searchBox').value;
+		console.log('The search word(s): ' + search_words); // for debugging
+	}
+};
